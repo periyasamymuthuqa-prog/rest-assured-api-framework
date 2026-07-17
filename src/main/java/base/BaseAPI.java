de96df767b5b47f499ccfa1b4ce9d7d2afe379f3
periyasamymuthuqa-prog <periyasamymuthu.qa@gmail.com>
2026-07-17 18:39:56 +0530

    Update BaseAPI.java

--- src/main/java/base/BaseAPI.java
+++ src/main/java/base/BaseAPI.java
@@ -1,1 +1,34 @@
+package base;
 
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+
+
+
+public class BaseAPI {
+
+
+    protected RequestSpecification request;
+
+
+
+    public BaseAPI(){
+
+
+        RestAssured.baseURI =
+                "https://reqres.in";
+
+
+        request =
+                RestAssured
+                .given()
+                .header(
+                "Content-Type",
+                "application/json");
+
+
+    }
+
+
+}
