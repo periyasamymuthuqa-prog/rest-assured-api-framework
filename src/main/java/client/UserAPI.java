20663be888337c98c01e0ccd1e3fc2dc115b7517
periyasamymuthuqa-prog <periyasamymuthu.qa@gmail.com>
2026-07-17 18:40:19 +0530

    Update UserAPI.java

--- src/main/java/client/UserAPI.java
+++ src/main/java/client/UserAPI.java
@@ -1,1 +1,78 @@
+package client;
 
+
+import base.BaseAPI;
+
+import io.restassured.response.Response;
+
+
+
+public class UserAPI extends BaseAPI {
+
+
+
+    public Response getUser(
+            int userId){
+
+
+        return request
+                .when()
+                .get(
+                "/api/users/"
+                + userId);
+
+    }
+
+
+
+
+    public Response createUser(
+            String payload){
+
+
+
+        return request
+                .body(payload)
+                .when()
+                .post(
+                "/api/users");
+
+
+    }
+
+
+
+
+    public Response updateUser(
+            int userId,
+            String payload){
+
+
+        return request
+                .body(payload)
+                .when()
+                .put(
+                "/api/users/"
+                + userId);
+
+
+    }
+
+
+
+
+    public Response deleteUser(
+            int userId){
+
+
+        return request
+                .when()
+                .delete(
+                "/api/users/"
+                + userId);
+
+
+    }
+
+
+}
