790eb62b1a8f9e09b9233a06e4e9661ff408d10e
periyasamymuthuqa-prog <periyasamymuthu.qa@gmail.com>
2026-07-17 18:40:56 +0530

    Create TestData.java

---
+++ src/main/java/utils/TestData.java
@@ -0,0 +1,21 @@
+package utils;
+
+
+public class TestData {
+
+
+    public static String createUserPayload(){
+
+
+        return """
+        {
+            "name": "Periyasamy",
+            "job": "Senior SDET"
+        }
+        """;
+
+
+    }
+
+
+}
